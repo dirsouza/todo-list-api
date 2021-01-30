1372883a4faecfa08fe728100a1f6716f638464e
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 13:50:59 -0400

    chore: adicionado nova regra de validação ao AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Rules\ValidIfNullOrFalseRule;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('valid_if_null_or_false', function ($attribute, $value, $parameters, $validator) {
+            list($field) = $parameters;
+            $fieldValue =Arr::get($validator->getData(), $field);
+            return (new ValidIfNullOrFalseRule($fieldValue))->passes($attribute, $value);
+        });
+
+        Validator::replacer('valid_if_null_or_false', function ($message, $attribute, $rule, $parameters, $validator) {
+            list($field) = $parameters;
+            $field = Arr::get($validator->customAttributes, $field);
+            return str_replace(':field', $field, $message);
+        });
     }
 }
