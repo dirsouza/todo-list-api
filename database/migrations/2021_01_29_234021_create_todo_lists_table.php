860b477432b2b3bffdd92a77f67b2aa23173403b
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 01:53:56 -0400

    chore: criado a migração da tabela

---
+++ database/migrations/2021_01_29_234021_create_todo_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->boolean('archived')->default(false);
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_list');
+    }
+}
