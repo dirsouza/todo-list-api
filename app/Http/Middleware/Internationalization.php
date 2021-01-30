b812c8535415c6991f8ffc9b546a292b82444af6
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 01:54:59 -0400

    chore: criado middleware para setar a internacionalização

---
+++ app/Http/Middleware/Internationalization.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class Internationalization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $locale = explode(',', $request->server('HTTP_ACCEPT_LANGUAGE'))[0];
+
+        if (!empty($locale)) {
+            App::setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
