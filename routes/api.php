6c46a7766df2b50b41f5815d9a2663f41d20ecec
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 01:53:30 -0400

    chore: criado as rotas

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', 'TodoListController@list')->name('tasks.list');
+        Route::post('create', 'TodoListController@create')->name('tasks.create');
+        Route::put('update/{id}', 'TodoListController@update')->name('tasks.update');
+        Route::delete('delete/{id}', 'TodoListController@delete')->name('tasks.delete');
+    });
+
+});
