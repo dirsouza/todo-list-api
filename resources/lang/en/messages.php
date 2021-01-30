585c8f3174f0d57a3664c54f9d76db0c17d33699
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 01:55:23 -0400

    chore: criado os arquivos de internacionalização

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'tasks' => [
+        'title' => 'Title',
+        'description' => 'Description',
+        'archived' => 'Archived',
+        'completed' => 'Completed',
+        'notFound' => 'No tasks found.',
+        'successRegister' => 'Task successfully registered.',
+        'errorRegister' => 'It was not possible to register the task.',
+        'successUpdate' => 'Task updated successfully.',
+        'errorUpdate' => 'The task could not be updated.',
+        'successDelete' => 'Task successfully deleted.',
+        'errorDelete' => 'The task could not be deleted.',
+        'validation' => [
+            'required' => 'The :attribute field is mandatory.',
+            'min' => 'The :attribute field must contain more than :min characters.',
+            'max' => 'The :attribute field must contain a maximum of :max characters.',
+            'boolean' => 'The :attribute field must be true or false.'
+        ]
+    ],
+];
---
+++ resources/lang/pt-BR/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'tasks' => [
+        'title' => 'Título',
+        'description' => 'Descrição',
+        'archived' => 'Arquivar',
+        'completed' => 'Completar',
+        'notFound' => 'Nenhuma tarefa encontrada.',
+        'successRegister' => 'Tarefa cadastrada com sucesso.',
+        'errorRegister' => 'Não foi possível realizar o cadastro da tarefa.',
+        'successUpdate' => 'Tarefa atualizada com sucesso.',
+        'errorUpdate' => 'Não foi possível realizar a atualização da tarefa.',
+        'successDelete' => 'Tarefa excluída com sucesso.',
+        'errorDelete' => 'Não foi possível realizar a exclusão da tarefa.',
+        'validation' => [
+            'required' => 'O campo :attribute é obrigatório.',
+            'min' => 'O campo :attribute deve conter mais de :min caracteres.',
+            'max' => 'O campo :attribute deve conter no máximo :max caracteres.',
+            'boolean' => 'O campo :attribute deve ser verdadeiro ou falso.'
+        ]
+    ],
+];
