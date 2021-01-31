bb7ac7f90d04c541533f8627320919dba8dcef99
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 23:41:27 -0400

    chore: adicionado documentação geral da API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,30 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version=L5_SWAGGER_INFO_VERSION,
+ *     title=L5_SWAGGER_INFO_TITLE,
+ *     description=L5_SWAGGER_INFO_DESCRIPTION,
+ *
+ *     @OA\Contact(
+ *          email=L5_SWAGGER_INFO_CONTACT
+ *     ),
+ *
+ *     @OA\License(
+ *          name=L5_SWAGGER_INFO_LICENSE_NAME,
+ *          url=L5_SWAGGER_INFO_LICENSE_URL
+ *     )
+ * )
+ *
+ * @OA\Host(
+ *     url=L5_SWAGGER_SERVER_HOST
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_SERVER_HOST
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
