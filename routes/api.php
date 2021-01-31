eb46d951303253dcc0903f6ef81a5fa9b1b3d850
Diogo Souza <diogo.souza@uds.com.br>
2021-01-30 23:42:40 -0400

    chore: adicionado rota para buscar uma tarefa pelo id

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('tasks')->group(function () {
         Route::get('/', 'TodoListController@list')->name('tasks.list');
+        Route::get('{id}', 'TodoListController@task')->name('tasks.task');
         Route::post('create', 'TodoListController@create')->name('tasks.create');
         Route::put('update/{id}', 'TodoListController@update')->name('tasks.update');
         Route::delete('delete/{id}', 'TodoListController@delete')->name('tasks.delete');
